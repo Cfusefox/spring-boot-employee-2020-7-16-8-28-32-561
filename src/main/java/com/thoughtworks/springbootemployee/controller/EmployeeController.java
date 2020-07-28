d5db9f9386e0b4b972cd5b75bf0134c7197c3e0c
fox <1036674617@qq.com>
2020-07-28 22:45:36 +0800

    [York] feature: add getCertainCompany function in EmployeeController

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,7 +1,9 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,19 @@ public class EmployeeController {
         employees.add(new Employee(2));
         employees.add(new Employee(3));
         return employees;
+    }
+
+    @GetMapping(path = "/{id}")
+    public Employee getCertainCompany(@PathVariable int id) {
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1));
+        employees.add(new Employee(2));
+        employees.add(new Employee(3));
+        for (Employee employee: employees) {
+            if(employee.getEmployeeID() == id) {
+                return employee;
+            }
+        }
+        return null;
     }
 }
