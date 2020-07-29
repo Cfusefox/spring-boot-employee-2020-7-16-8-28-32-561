bb37ad053575d741aa3e573bda719d81d1c0fb09
ZHANGYO5 <york.zhang@oocl.com>
2020-07-29 12:37:51 +0800

    [York] refactor: refactor getEmployeesInformation

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,40 +1,31 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.data.EmployeeData;
 import com.thoughtworks.springbootemployee.model.Employee;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
 public class EmployeeController {
+
+    List<Employee> allEmployee = new EmployeeData().getEmployees();
+
     @GetMapping
     public List<Employee> getEmployeesInformation(@RequestParam(name = "page" , required = false) Integer page,
                                  @RequestParam(name = "pageSize",required = false) Integer pageSize,
                                  @RequestParam(name =  "gender", required = false) String gender) {
-        List<Employee> employees = new ArrayList<>();
-        List<Employee> certainEmployees = new ArrayList<>();
-        employees.add(new Employee(1, "Zach", 18, "male", 1000));
-        employees.add(new Employee(2, "York", 18, "male", 1000));
-        employees.add(new Employee(3, "Karen", 18, "female", 1000));
-        if(page != null && pageSize != null) {
-            for(int index = 0; index < employees.size(); index++) {
-                if(index >= (page - 1) && certainEmployees.size() <pageSize) {
-                    certainEmployees.add(employees.get(index));
-                }
-            }
-            return certainEmployees;
-        }
+        List<Employee> certainEmployees = allEmployee;
         if(gender != null) {
-            for (Employee employee: employees) {
-                if(employee.getGender().equals(gender)) {
-                    certainEmployees.add(employee);
-                }
-            }
-            return certainEmployees;
+            certainEmployees = certainEmployees.stream().filter(certainEmployee -> certainEmployee.getGender().equals(gender)).collect(Collectors.toList());
         }
-        return employees;
+        if(page != null && pageSize != null) {
+            certainEmployees = certainEmployees.stream().skip(page - 1).limit(pageSize).collect(Collectors.toList());
+        }
+        return certainEmployees;
     }
 
     @GetMapping(path = "/{id}")
