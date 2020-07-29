813d1812672027c288ef106f7ec4e50224ae228e
ZHANGYO5 <york.zhang@oocl.com>
2020-07-29 08:38:37 +0800

    [York] feature: add CompanyData

---
+++ src/main/java/com/thoughtworks/springbootemployee/data/CompanyData.java
@@ -0,0 +1,23 @@
+package com.thoughtworks.springbootemployee.data;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanyData {
+    List<Company> companies = new ArrayList<>();
+
+    public CompanyData() {
+        companies.add(new Company(1, null));
+        companies.add(new Company(2, null));
+        companies.add(new Company(3, null));
+        companies.add(new Company(4, null));
+        companies.add(new Company(5, null));
+    }
+
+    public List<Company> getCompanies() {
+        return companies;
+    }
+}
