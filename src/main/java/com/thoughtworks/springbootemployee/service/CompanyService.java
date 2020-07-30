1659df0c89295cfd95c97a2ec5b3ffc474120398
ZHANGYO5 <york.zhang@oocl.com>
2020-07-30 08:44:19 +0800

    [York] refactor: rename companyResposition to companyReposition

--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -10,46 +10,46 @@ import java.util.List;
 import java.util.Objects;
 
 public class CompanyService {
-    CompanyRepository companyRespository;
+    CompanyRepository companyRepository;
 
-    public CompanyService(CompanyRepository companyRespository) {
-        this.companyRespository = companyRespository;
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
     }
 
     public List<Company> findAll(){
-        return  companyRespository.findAll();
+        return  companyRepository.findAll();
     }
 
     public Company findCompanyByID(int id) {
-        return this.companyRespository.findById(id).orElse(null);
+        return this.companyRepository.findById(id).orElse(null);
     }
 
     public List<Employee> findCompanyEmployeesByID(int companyID) {
-        return Objects.requireNonNull(companyRespository.findById(companyID).orElse(null)).getEmployees();
+        return Objects.requireNonNull(companyRepository.findById(companyID).orElse(null)).getEmployees();
     }
 
     public Page<Company> findRangeOfCompany(int page, int pageSize) {
-        return this.companyRespository.findAll(PageRequest.of(page, pageSize));
+        return this.companyRepository.findAll(PageRequest.of(page, pageSize));
     }
 
     public Company addCompany(Company company) {
-        companyRespository.save(company);
+        companyRepository.save(company);
         return company;
     }
 
     public Company deleteCompany(int companyID) {
         Company company = findCompanyByID(companyID);
-        companyRespository.deleteById(companyID);
+        companyRepository.deleteById(companyID);
         return company;
     }
 
     public Company update(int id, Company oocl) {
-        Company updateCompany = this.companyRespository.findById(id).orElse(null);
+        Company updateCompany = this.companyRepository.findById(id).orElse(null);
         assert updateCompany != null;
         updateCompany.setCompanyName(oocl.getCompanyName());
         updateCompany.setEmployees(oocl.getEmployees());
         updateCompany.setEmployeesNumber(oocl.getEmployeesNumber());
-        this.companyRespository.save(updateCompany);
+        this.companyRepository.save(updateCompany);
         return updateCompany;
     }
 }
